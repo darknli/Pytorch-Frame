2ad0dcfeeca2b0d3c93bc40f2bea83a03c13ab32
darknli <1187220556@qq.com>
2023-12-27 22:57:01 +0800

    AccelerateTrainer加入ema和梯度累计

--- torch_frame/accelerate_trainer.py
+++ torch_frame/accelerate_trainer.py
@@ -7,7 +7,7 @@ import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from typing import Optional, Union, List, Dict, Any
-from .trainer import Trainer, MetricStorage, osp
+from .trainer import Trainer, MetricStorage, EMA
 from .hooks import CheckpointerHook, HookBase, LoggerHook
 from .utils.misc import get_workspace
 from .lr_scheduler import LRWarmupScheduler
@@ -27,6 +27,7 @@ class AccelerateTrainer(Trainer):
          * "no", 不使用amp技术
          * "fp16"
          * "bf16", 30系及以后的卡型才能使用
+    gradient_accumulation_steps: int, default 1. 梯度累计数，显存不够用又需要大batch的时候可以加大数值
     """
 
     def __init__(
@@ -45,6 +46,7 @@ class AccelerateTrainer(Trainer):
             hooks: Optional[List[HookBase]] = None,
             use_ema: bool = False,
             ema_decay: float = 0.9999,
+            gradient_accumulation_steps: int = 1,
             create_new_dir: Optional[str] = "time_s"
     ):
         self.work_dir = get_workspace(work_dir, create_new_dir)
@@ -52,6 +54,7 @@ class AccelerateTrainer(Trainer):
         self.accelerator = Accelerator(
             mixed_precision=mixed_precision,
             project_config=accelerator_project_config,
+            gradient_accumulation_steps=gradient_accumulation_steps
         )
 
         self.weight_dtype = torch.float32
@@ -63,7 +66,9 @@ class AccelerateTrainer(Trainer):
         self.model = model
         if use_ema:
             warnings.warn("accelerate版暂不支持ema, 敬请期待")
-        self.model_ema = None
+            self.model_ema = EMA(self.model_or_module, ema_decay)
+        else:
+            self.model_ema = None
         self.optimizer = optimizer
         # convert epoch-based scheduler to iteration-based scheduler
         if isinstance(lr_scheduler, str):
@@ -135,50 +140,53 @@ class AccelerateTrainer(Trainer):
         .. Note::
             标准的学习率调节器是基于epoch的, 但torch_frame框架是基于iter的, 所以它在每次iter之后都会调用
         """
-        #####################
-        # 1. 计算loss #
-        #####################
-        loss_info = self.model(batch)
-        if isinstance(loss_info, torch.Tensor):
-            losses = loss_info
-            loss_info = {"total_loss": loss_info}
-        elif isinstance(loss_info, tuple):
-            assert len(loss_info) == 2, "loss_info需要是一个二元组，第一个是需要反向传播的，第二个是其他参考指标"
-            backward_params, metric_params = loss_info
-            assert isinstance(metric_params, dict), "loss_info的第二个值需要是dict类型"
-            if isinstance(backward_params, torch.Tensor):
-                losses = backward_params
-                metric_params["total_loss"] = backward_params
-                loss_info = metric_params
-            elif isinstance(backward_params, dict):
-                losses = sum(backward_params.values())
-                backward_params["total_loss"] = losses
-                backward_params.update(metric_params)
-                loss_info = backward_params
-        else:
-            assert "total_loss" not in loss_info, "当model返回是一个dict的时候不可以传出包含"
-            losses = sum(loss_info.values())
-            loss_info["total_loss"] = losses
+        # 这里貌似只支持一个模型, 如果类似SD的训练任务可能需要把text_encoder和unet都包在一个模型里
+        with self.accelerator.accumulate(self.model):
+            #####################
+            # 1. 计算loss #
+            #####################
+            loss_info = self.model(batch)
+            if isinstance(loss_info, torch.Tensor):
+                losses = loss_info
+                loss_info = {"total_loss": loss_info}
+            elif isinstance(loss_info, tuple):
+                assert len(loss_info) == 2, "loss_info需要是一个二元组，第一个是需要反向传播的，第二个是其他参考指标"
+                backward_params, metric_params = loss_info
+                assert isinstance(metric_params, dict), "loss_info的第二个值需要是dict类型"
+                if isinstance(backward_params, torch.Tensor):
+                    losses = backward_params
+                    metric_params["total_loss"] = backward_params
+                    loss_info = metric_params
+                elif isinstance(backward_params, dict):
+                    losses = sum(backward_params.values())
+                    backward_params["total_loss"] = losses
+                    backward_params.update(metric_params)
+                    loss_info = backward_params
+            else:
+                assert "total_loss" not in loss_info, "当model返回是一个dict的时候不可以传出包含"
+                losses = sum(loss_info.values())
+                loss_info["total_loss"] = losses
 
-        ##########################
-        # 2. 计算梯度 #
-        ##########################
-        self.optimizer.zero_grad()
-        self.accelerator.backward(losses)
-        if self._clip_grad_norm > 0 and self.accelerator.sync_gradients:
-            self.accelerator.clip_grad_norm_(self.model.parameters(), self._clip_grad_norm)
+            ##########################
+            # 2. 计算梯度 #
+            ##########################
+            self.optimizer.zero_grad()
+            self.accelerator.backward(losses)
+            if self._clip_grad_norm > 0 and self.accelerator.sync_gradients:
+                self.accelerator.clip_grad_norm_(self.model.parameters(), self._clip_grad_norm)
 
-        ##############################
-        # 3. 更新模型参数 #
-        ##############################
-        self.optimizer.step()
+            ##############################
+            # 3. 更新模型参数 #
+            ##############################
+            self.optimizer.step()
+
+            ###########################
+            # 4. 调整学习率 #
+            ###########################
+            self.lr_scheduler.step()
+
         if self.model_ema:
             self.model_ema.update(self.model)
-
-        ###########################
-        # 4. 调整学习率 #
-        ###########################
-        self.lr_scheduler.step()
 
         show_info = {k: v.detach().cpu().item() if isinstance(v, torch.Tensor) else v for k, v in loss_info.items()}
         show_info = dict(sorted(show_info.items(), key=lambda x: x[0] != "total_loss"))  # 保证total_loss在最后一位
